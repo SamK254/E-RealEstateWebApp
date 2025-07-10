bba261ac7b387af70268f714e8e7fd2037fa4f97
Samuel Kariuki <sndonga@kabarak.co.ke>
2025-07-10 16:52:33 +0300

    adding vercel commit

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { createUser } from "../controllers/userCntrl.js";
+import { bookVisit, cancelBooking, createUser, getAllBookings, getAllFavourites, toFav } from "../controllers/userCntrl.js";
+import jwtCheck from "../config/auth0Config.js";
 
 const router = express.Router();
 
-router.post("/register", createUser);
+router.post("/register", jwtCheck, createUser);
+router.post("/bookVisit/:id", jwtCheck,bookVisit);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id",jwtCheck, cancelBooking);
+router.post("/toFav/:rid",jwtCheck, toFav);
+router.post("/allFav", jwtCheck, getAllFavourites);
+
+
+
+
 
 export { router as userRoute };
